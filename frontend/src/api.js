7711ec281b53b240d55bba45c5c5e0adc6c514c5
turne309 <jaydonmturner@outlook.com>
2025-07-31 14:45:50 -0400

    Added backedn URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
+// Thisis the url that connects our frontend to our backend in Choreo
+const apiUrl = "/choreo-apis/django-react-boilerplate/backend/v1";
+
 const api = axios.create({
   // Importing base url from .env variable
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 });
 
 api.interceptors.request.use(
